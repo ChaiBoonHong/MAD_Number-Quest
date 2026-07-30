322344bf4a42173ac23aba6094396fab3f9e8b75
CHAI BOON HONG <iamboonnnnn@1utar.my>
2026-07-30 14:39:58 +0800

    Initial commit

---
+++ app/src/androidTest/java/com/uccd3223/p1_chai_boon_hong_2206806/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package com.uccd3223.p1_chai_boon_hong_2206806
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.uccd3223.p1_chai_boon_hong_2206806", appContext.packageName)
+    }
+}
